cd7d43d87f5bdc0260e03cadb045c04fb200083d
dsjav <jportelagarcia@danielcastelao.org>
2024-12-14 16:18:14 +0100

    Putin

--- ejemplo.java
+++ ejemplo.java
@@ -1,5 +1,6 @@
 public class ejemplo {
     public static void main (String []args){
-        //ejemplo de README en Github
+        System.out.println("Esto es un mensaje para Putin");
+        System.out.println("Kim Jong Un aprueba este commit");
     }
 }
